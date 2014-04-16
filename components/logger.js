597fa06c1f6989d32b07c5845f7b5dc0897d9f93
ericsims(laptop) <EricSimsM@gmail.com>
2014-04-16 19:41:29 -0400

    added log clearing

--- components/logger.js
+++ components/logger.js
@@ -11,25 +11,41 @@ exports.init = function (settings) {
 		console.log("could not open file: " + name);
 };
 
-//Test logger... comment out for release
-exports.init({log: {file: "logs/test", appendDate: 1, extension: null}});
-
-if (stream)
-	stream.once('open', function(fd) {
-		stream.write("My first row\n");
-		stream.write("My second row\n");
-		//stream.end();
-	});
-
 exports.write = function (str) {
-	if (stream) {
-		setTimeout(function() {
-			stream.write(str.toString() + "\n");
-			//stream.end();
-		},100);
-	}
+	if (stream)
+		stream.write(str.toString() + "\n");
 };
 
-//more test code
-for (var i = 0; i < 100000; i++)
-	exports.write(i);
+exports.close = function() {
+	if (stream)
+		stream.end();
+};
+
+
+//test code via arg '-test'
+if (process.argv.slice(2)[0] == '-test') {
+	exports.init({log: {file: "logs/test", appendDate: 1, extension: null}});
+	if (stream)
+		stream.once('open', function(fd) {
+			stream.write("foo");
+			stream.write("bar");
+		});
+	for (var i = 0; i < 10; i++)
+		exports.write(i);
+};
+
+if (process.argv.slice(2)[0] == '-clear') {
+	if (path = process.argv.slice(2)[1]) {
+		if (process.argv.slice(2)[2] == "-a") {
+			fs.readdirSync(path).forEach(function(file, index){
+				var curPath = path + "/" + file;
+				fs.unlinkSync(curPath);
+			});
+			console.log('successfully deleted ' + path + "/*");
+		} else {
+			fs.unlinkSync(path);
+			console.log('successfully deleted ' + path);
+		}
+	} else
+		console.log("no path specified!");
+};

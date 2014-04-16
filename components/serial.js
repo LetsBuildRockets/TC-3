899cba883ba90c79cce736c0b729bead435285a3
Eric Sims <EricSimsM@gmail.com>
2014-04-15 23:15:56 -0400

    ready to test on pi! almost working on w7

--- components/serial.js
+++ components/serial.js
@@ -1,17 +1,17 @@
 var yaml = require('js-yaml');
 var fs = require('fs');
-var serialPort = require("serialport");
-var SerialPort = serialPort.SerialPort;
+var SerialPort = require("serialport").SerialPort;
 var settings = yaml.safeLoad(fs.readFileSync('./config/settings.yaml', 'ascii'));
 
 //list ports for rpi
-serialPort.list(function (err, ports) {
+//lol i dont think i need this
+/*serialPort.list(function (err, ports) {
 	ports.forEach(function(port) {
 		console.log(port.comName);
 		console.log(port.pnpId);
 		console.log(port.manufacturer);
 	});
-});
+});*/
 
 
 var serialPort = new SerialPort(settings.serial.port, {
@@ -20,15 +20,15 @@ var serialPort = new SerialPort(settings.serial.port, {
 
 serialPort.open(function () {
 	console.log('open');
-	serialPort.on('data', function(data) {
+	/*serialPort.on('data', function(data) {
 		console.log('data received: ' + data);
-	});
-	
+	});*/
+
 	var last = 0;
-	setTimeout(function(){
+	setTimeout(function() {
 		if (last)
 			serialPort.write("0:0\n");
 		else
 			serialPort.write("0:1\n");
-	},1000);
+	}, 500);
 });

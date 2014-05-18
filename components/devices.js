b172654bb908131b89e3ef05327ff83e637323a1
Eric Sims <EricSimsM@gmail.com>
2014-05-18 10:43:23 -0400

    started, but don't know the name for calc stuff

--- components/devices.js
+++ components/devices.js
@@ -35,28 +35,9 @@ function extract(array, object) {
 	return vals;
 }
 
-function integrateDevices(){
-	for(var i = 0; i < device.length; i++) {
-		if(device[i].integrand) {
-			var integrand = parseInt(device[i].integrand);
-			if(!device[i].value) device[i].value = 0; //this is dumb...
-			device[i].value += (((device[integrand].value + device[integrand].lastValue) / 2) * (sequencer.time() - device[integrand].lastUpdate));
-			if(settings.debug) console.log("integrand: " + integrand + " integtal: " + device[i].value);
-		}
-	}
-	cacheDeviceData();
-}
-
-function cacheDeviceData() {
-	for(var i = 0; i < device.length; i++) {
-		device[i].lastUpdate = sequencer.time();
-		device[i].lastValue = device[i].value;
-	}
-}
-
 exports.updateDevice = function (newdevice) {
-	device[0].value = newdevice[0].value;
-	integrateDevices();
+	device[0].value = newdevice[0].value; //TODO: replace with proper update. Not just device 0.
+	calculateSpecialDevices();
 };
 
 exports.startCountdown = function() {
@@ -65,5 +46,41 @@ exports.startCountdown = function() {
 		logger.logArray(sequencer.time(), extract(device, 'value'));
 	},100);
 };
+
+//calculations for integrals and derivatives
+function calculateSpecialDevices() {
+	derivateDevices();
+	integrateDevices();
+	cacheDeviceData();
+}
+
+function integrateDevices() {
+	for(var i = 0; i < device.length; i++) {
+		if(device[i].integrand) {
+			var integrand = parseInt(device[i].integrand);
+			if(!device[i].value) device[i].value = 0; //this is dumb...
+			device[i].value += (((device[integrand].value + device[integrand].lastValue) / 2) * (sequencer.time() - device[integrand].lastUpdate));
+			if(settings.debug) console.log("integrand: " + integrand + " integral: " + device[i].value);
+		}
+	}
+}
+
+function derivateDevices() {
+	for(var i = 0; i < device.length; i++) {
+		if(device[i].integrand) {
+			var integrand = parseInt(device[i].integrand);
+			if(!device[i].value) device[i].value = 0; //this is dumb...
+			device[i].value += (((device[integrand].value + device[integrand].lastValue) / 2) * (sequencer.time() - device[integrand].lastUpdate));
+			if(settings.debug) console.log("integrand: " + integrand + " integtal: " + device[i].value);
+		}
+	}
+}
+
+function cacheDeviceData() {
+	for(var i = 0; i < device.length; i++) {
+		device[i].lastUpdate = sequencer.time();
+		device[i].lastValue = device[i].value;
+	}
+}
 
 exports.device = device;

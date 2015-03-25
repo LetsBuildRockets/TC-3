b4a519779f691d6e8eaf3c748d7b7a13f354b15b
Eric Sims <ericsimsm@gmail.com>
2015-03-25 12:08:58 -0400

    organizing pinmodes

--- atmega_interface/Shifter.cpp
+++ atmega_interface/Shifter.cpp
@@ -1,37 +1,33 @@
 int clockPin = 11;
-int dataPin = 12;
-int dataPin2 = 10;
+int dataPins = {
+  12, 10, 0, 0};
 int latchPin = 9;
-
-boolean output[16] = { };
+boolean output[16] = { 
+};
 boolean stringComplete = false;
 String command = "";
 int address = -1;
 
 void setup() {
   Serial.begin(9600);
-  
-  pinMode(dataPin, OUTPUT);
-  pinMode(dataPin2, OUTPUT);
+
+  for(int i = 0; i < 4; i++) {
+    pinMode(dataPin[i], OUTPUT);
+  }
   pinMode(clockPin, OUTPUT);
   pinMode(latchPin, OUTPUT);   
-  
+
   command.reserve(200);
-  
+
   for (int i = 0; i <= sizeof(output)/sizeof(boolean); i++) {
     output[i] = 0;
   }  
   shiftDataOut(dataPin, output);
-  
-  
+
+
   Serial.println("initialized");
 }
 
-int freeRam () {
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-}
 
 
 void serialEvent() {
@@ -47,12 +43,12 @@ void serialEvent() {
     } 
     if (inChar == '\n' && address > -1) {
       boolean state = (command.toInt() == 1);
-      
+
       Serial.println(String(address) + ";" + String(state));
       //Serial.println(command);
       stringComplete = true;
       updateShiftRegister(address, state);
-      
+
       //Serial.print("freeRAM=");
       //Serial.println(freeRam());
       address = -1;
@@ -60,12 +56,12 @@ void serialEvent() {
     command += inChar;
   }
 }
-     
+
 void updateShiftRegister(int address, boolean state) {
   output[address] = state;
   shiftDataOut(dataPin, output);
 }
- 
+
 void shiftDataOut(uint8_t dataPin, boolean output[]) {
   int i;
   for (i = 7; i >= 0; i--)  {
@@ -74,6 +70,8 @@ void shiftDataOut(uint8_t dataPin, boolean output[]) {
     digitalWrite(clockPin, HIGH);
     digitalWrite(clockPin, LOW);
   }
-    digitalWrite(latchPin, HIGH);
-    digitalWrite(latchPin,LOW);
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin,LOW);
 }
+
+

7fa22d6eaa2aeb70b39849da8e977d235b7b6e0f
Eric Sims <ericsimsm@gmail.com>
2015-04-06 14:28:45 -0400

    implementing write functions

--- components/i2c.js
+++ components/i2c.js
@@ -1,6 +1,9 @@
-var i2c = require('i2c');
-var address = 0x18;
-var wire = new i2c(address, {device: '/dev/i2c-1'});
+var isPi = require('os').arch()  == "arm";
+if(isPi) {
+  var i2c = require('i2c');
+  var wire = new i2c(0x18, {device: '/dev/i2c-1'});
+}
+var deviceAddr = [0x48, 0x49, 0x50, 0x51];
 var updateDevice;
 var settings;
 
@@ -13,13 +16,26 @@ exports.init = function(newSettings, newUpdate) {
 exports.scan  = function () {
   wire.scan(function(err, data) {
     if(err) console.log(data);
-    console.log(data);  
+    console.log(data);
   });
 };
+
+function readSensorData () {
+  if(isPi) {
+    wire.setAddress(deviceAddr[1]);
+    
+  } else {
+    console.log("Cannot Read Sensor Data. This is not a Raspberry Pi-.");
+    return 0;
+  }
+}
+
+
 
 exports.write = function (address, state) {
   // TODO: plain write
 };
+
 
 var lastPulse = false;
 exports.pulse = function(address, wavelength){
@@ -32,21 +48,5 @@ exports.pulse = function(address, wavelength){
 
 // test code via arg -test
 if (process.argv.slice(2)[0] == '-test') {
-	exports.init({serial: {port: process.argv.slice(2)[1], baudrate: 9600}});
-
-	serialPort.on('data', function(data) {
-		console.log('data received: ' + data);
-	});
-
-	var last = 0;
-	setInterval(function() {
-		if (serialPortExists) {
-			if (last)
-				serialPort.write("0:0\n");
-			else
-				serialPort.write("0:1\n");
-			last = !last;
-		} else
-			console.log("serial port not open!!!");
-	}, 500);
+  readSensorData();
 }

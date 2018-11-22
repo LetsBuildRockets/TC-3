7ac49aa5a2d96fe50226527be692e3e41df7eadc
Eric Sims <EricSimsM@gmail.com>
2018-11-22 15:14:19 -0500

    cleaned up plotting tool

--- DAQ/plotdat.m
+++ DAQ/plotdat.m
@@ -1,16 +1,19 @@
 hold off
 clf('reset')
-[data text]=importdata('data/dat_80.csv');
+[data text]=importdata('data/dat_71.csv');
 x= str2double(data.textdata(:,1));
 x=x-x(2);
-len = 10;
+len = 1;
 b = ones(len,1)/len;
-m = [6,14,16];
+m = [6,8,14,16,13];
+rangeX = [0, inf];
+fprintf("range: %.2f sec\n",rangeX(2)-rangeX(1));
 for n = m
     col1 = data.data(:, n+1);
-    I = ~isnan(col1) & ~isnan(x);
+    I = ~isnan(col1) & ~isnan(x)& rangeX(1)<x & x<rangeX(2);
     hold on
     plot(x(I), filter(b,1,flip(filter(b,1,flip(col1(I))))) ,'-')
+    fprintf("mean col %d: %.2f\n",n,mean(col1(I)));
 end
 axis tight
 legend(string(m))

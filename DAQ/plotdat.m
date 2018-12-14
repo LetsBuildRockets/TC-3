a300d4f0e56016dd292f3c80cac267880320b8fb
Eric Sims <EricSimsM@gmail.com>
2018-12-13 23:53:50 -0500

    idk test data

--- DAQ/plotdat.m
+++ DAQ/plotdat.m
@@ -1,13 +1,16 @@
 hold off
 clf('reset')
-[data text]=importdata('data/dat_80.csv');
+[data text]=importdata('data/dat_89.csv');
 x= str2double(data.textdata(:,1));
 x=x-x(2);
-len = 10;
+len = 1;
 b = ones(len,1)/len;
-m = [6,14,16];
+m = [6,14,16,5,15,12];
 for n = m
     col1 = data.data(:, n+1);
+    if n == 12
+        col1 = col1*20;
+    end
     I = ~isnan(col1) & ~isnan(x);
     hold on
     plot(x(I), filter(b,1,flip(filter(b,1,flip(col1(I))))) ,'-')
